4b061c497f08ebfa13e812b76deba6da30683d55
PoojaJTechno <poojajtechnocredits@gmail.com>
2022-01-23 16:42:56 +0530

    Project upload

---
+++ src/P1/Test1.java
@@ -0,0 +1,9 @@
+package P1;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		System.out.println("In P1 Test1");
+	}
+
+}
---
+++ src/P2/Test2.java
@@ -0,0 +1,9 @@
+package P2;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		System.out.println("In P2 Test2");
+	}
+
+}
